e26d2e264b9d9b04b4c91bdca74004edc03cb2b6
grammlich <peter.grammlich@rwth-aachen.de>
2017-07-19 16:12:52 +0200

    Added Detail

--- ComicBook/ComicBook/Controllers/ComicBooksController.cs
+++ ComicBook/ComicBook/Controllers/ComicBooksController.cs
@@ -6,8 +6,24 @@ using System.Web;
 
 namespace ComicBook.Controllers
 {
-    public class ComicBooksController : Controller 
+    public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                return Redirect("/");
+            }
+            
+             return Content("Hello from the Comic Books Controller");
+               
+                
+            
+
+            
+
+
+        }
 
     }
 }
